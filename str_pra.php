f22da975f9bd34d0985d7a5ccde636c0e57904e7
linkzepp <adriftchen@gmail.com>
2020-10-29 10:46:27 +0800

    字串四個練習

--- str_pra.php
+++ str_pra.php
@@ -41,21 +41,23 @@ echo $result;
 ?>
 <h3>sol2</h3>
 <?php
-str_repeat("*",mb_strlength(aaddw1123));
+echo str_repeat("*",mb_strlen('aaddw1123'));
 ?>
 
 <h1>將”this,is,a,book”依”,”切割後成為陣列</h1>
 <?php
-print_r(explode(',','this is a book'));
-
+print_r(explode(',','this,is,a,book'));
 ?>
+</pre>
 
 <h1>將上例陣列重新組合成“this is a book”</h1>
 <?php
-echo implode(" ",explode(',','this is a book'));
+
+echo implode(" ",explode(',','this,is,a,book'));
+
 ?>
 
 <h1>將” The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…”</h1>
 <?php
-echo mb_substr('The reason why a great man is great is that he resolves to be a great man',0,10)
+echo mb_substr('The reason why a great man is great is that he resolves to be a great man',0,10). '...';
 ?>
